09b13bf7b62ead362e20436d928dcb74605e3183
Tiemi Faustino <hayashipharma@gmail.com>
2021-12-02 18:04:35 -0300

    Requisito 5 ok

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,9 @@ function concatName(arrayStrings) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let score = wins * 3 + ties;  
+  return score;
 }
 
 // Desafio 6
